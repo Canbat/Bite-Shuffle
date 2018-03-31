6320013cfdeea978f59c87e96f008abfaa6e4a54
skibbidyska <dustin.wright3@gmail.com>
2018-03-31 13:11:14 -0400

    info modal added

--- assets/javascript/modal2.js
+++ assets/javascript/modal2.js
@@ -1,1 +1,10 @@
-a
+$(document).ready(function () {
+    $('#restaurant-info')
+        .modal('attach events', '#bite', 'show')
+    ;
+    $("#menu-info").hide();
+
+    $("#menu").on("click", function(){
+        $("#menu-info").toggle();
+    })
+})
