877e21ae4c7029f907c9e4240fd0f86f0c0bf858
Eric "ET" Thornton <firmonomics@gmail.com>
2018-04-03 18:48:13 -0400

    Logging zomato API to console successful for Storing the restaurant data(Restaurant Name, Cuisine info, User Rating, Price, Featured Image).

--- assets/javascript/zomato.js
+++ assets/javascript/zomato.js
@@ -29,4 +29,55 @@ var apiUrl = "https://developers.zomato.com/api/v2.1/search?";
     }
   });
 
-  
+//Creating an AJAX call to randomly generate the Restaurant Name & Restarant Image.
+
+function displayRestaurantData(){
+
+    $.ajax({
+        url: apiUrl,
+        headers: {
+            'user-key': userKey,
+            'Content-Type':'application/json'
+        },
+        method: 'GET',
+        dataType: 'json',
+        //could use .then(function(reponse or data)) 
+        success: function(data){
+        
+            // Storing the restaurant data(Restaurant Name, Cuisine info, User Rating, Price, Featured Image)
+            var restaurant = data.restaurants[0].restaurant.name;
+            var cuisine = data.restaurants[0].restaurant.cuisines;
+            var rating = data.restaurants[0].restaurant.user_rating.aggregate_rating;
+            var price = data.restaurants[0].restaurant.price_range;
+            var photo = data.restaurants[0].restaurant.featured_image;
+            console.log(restaurant);
+            console.log(cuisine);
+            console.log("Bite Rating " + rating);
+            console.log("Price " + price);
+            console.log(photo);
+        },
+        //if you use .then(function(response or data)) you would use .catch(function(xml,text,error)) instead of error.
+        error: function(xml,text,error){
+            console.log(xml,text,error);
+        }
+      });
+
+
+}  displayRestaurantData()
+
+//Randomize restaurant data by looping through api restaurant array to choose restaurant based on matching cuisine selections & zipcode.
+
+
+
+// Create jquery logic for user onClick Nope! & Bite! commands
+
+    //Restaurant Name onClick logic
+
+
+    //Cuisine data onClick logic
+    
+
+    //User Rating display
+
+
+    //Average Price display
