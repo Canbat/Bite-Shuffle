38b27bed7ccae1081946b01ab90a982b400e890d
Eric "ET" Thornton <firmonomics@gmail.com>
2018-04-05 18:58:05 -0400

    Created changes to README, added some styling, created the Bite-Shuffle logo, updated zomato api call so that there is a temporary place holder image.

--- assets/javascript/zomato.js
+++ assets/javascript/zomato.js
@@ -1,3 +1,4 @@
+
 var lat;
 var lng;
 var restaurantLat;
@@ -20,6 +21,8 @@ $(document).ready(function () {
 
 });
 
+
+
 var userKey = "74947a847a6bedf2fed11165ada8f8b4";
 
 // var apiUrl = "https://developers.zomato.com/api/v2.1/cuisines?city_id=35&lat=33.7490&lon=84.3880";
@@ -28,27 +31,29 @@ var userKey = "74947a847a6bedf2fed11165ada8f8b4";
 // var apiUrl = "https://developers.zomato.com/api/v2.1/search?q=atlanta";
 
 //Global Zomato Search ---Use this one!!!
-var apiUrl;
 
-//  $.ajax({
-//   url: apiUrl,
-//   headers: {
-//       'user-key': userKey,
-//       'Content-Type':'application/json'
-//   },
-//   method: 'GET',
-//   dataType: 'json',
-//   //could use .then(function(reponse or data))
-//   success: function(data){
-//   //   console.log('succes: '+data);
-//   console.log(data);
-//   },
-//   //if you use .then(function(response or data)) you would use .catch(function(xml,text,error)) instead of error.
-//   error: function(xml,text,error){
-//       console.log(xml,text,error);
-//   }
-// });
-//menu items
+var apiUrl = "https://developers.zomato.com/api/v2.1/search?";
+
+//    $.ajax({
+//     url: apiUrl,
+//     headers: {
+//         'user-key': userKey,
+//         'Content-Type':'application/json'
+//     },
+//     method: 'GET',
+//     dataType: 'json',
+//     //could use .then(function(reponse or data)) 
+//     success: function(data){
+//     //   console.log('succes: '+data);
+//     console.log(data);
+//     },
+//     //if you use .then(function(response or data)) you would use .catch(function(xml,text,error)) instead of error.
+//     error: function(xml,text,error){
+//         console.log(xml,text,error);
+//     }
+//   });
+
+
 // Generating random restaurant number = R
 var R = Math.floor(Math.random() * 20) - 1;
 console.log(R);
@@ -70,25 +75,36 @@ function displayRestaurantData() {
 
             // Storing the restaurant data(Restaurant Name, Cuisine info, User Rating, Price, Featured Image)
             var restaurant = data.restaurants[R].restaurant.name;
-            var restaurantName = $('#restaurant-name').text(restaurant);
+                var restaurantName = $('#restaurant-name').text(restaurant);
+
             var cuisine = data.restaurants[R].restaurant.cuisines;
+
             var rating = data.restaurants[R].restaurant.user_rating.aggregate_rating;
-            var userRating = $('#zomato-rating').text("Bite-Rating: " + rating);
+                var userRating = $('#zomato-rating').text("Bite-Rating: "+ rating);
+
             var price = data.restaurants[R].restaurant.price_range;
-            var userRating = $('#price-scale').text(price + " out of 5" + " :" + "Price-Range ");
+                var userRating = $('#price-scale').text(price + " out of 5" + " :" + "Price-Range " );
+
             var photo = data.restaurants[R].restaurant.featured_image;
-            $('#restaurant-img').attr('src', photo);
-            restaurantLat = data.restaurants[R].restaurant.location.latitude;
-            restaurantLng = data.restaurants[R].restaurant.location.longitude;
-            initMap();
+                
+                if (photo == ""){
+                    $('#restaurant-img').attr('src', assets/images/biteshuffle.png);
+                }
+                else {
+                $('#restaurant-img').attr('src', photo);
+                }
 
             console.log(restaurant);
             console.log(cuisine);
             console.log("Bite-Rating " + rating);
             console.log("Price-Range " + price);
             console.log(photo);
+
             console.log(restaurantLat);
             console.log(restaurantLng);
+
+            console.log(data);
+
         },
         //if you use .then(function(response or data)) you would use .catch(function(xml,text,error)) instead of error.
         error: function (xml, text, error) {
